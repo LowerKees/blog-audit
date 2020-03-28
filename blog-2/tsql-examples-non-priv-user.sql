8e106cd97161958caf0388b772917adce9ba6454
Martijn Beenker <martijn.beenker@avanade.com>
2020-03-28 16:49:17 +0100

    Removing unnecessary statements from sql script

--- blog-2/tsql-examples-non-priv-user.sql
+++ blog-2/tsql-examples-non-priv-user.sql
@@ -1,9 +1,2 @@
 ALTER USER TEST_USER_01 WITH PASSWORD = 'C0mpl3xP@sswordsSh0uldBeC0mpl3X!!!ilpohnbvfgtyjkiuy' -- Executed as the non-priviledged account TEST_USER_02
 GRANT CONTROL TO TEST_USER_01;
-
-INSERT INTO dbo.TestTable (
-	FirstName, LastName, Age
-)
-VALUES (
-	'TestUser01', 'This is a test value', 30
-);
